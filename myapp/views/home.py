f67dbdadd1ea02a5d902a2e8a750330984a59802
Sara Itani <itani@mit.edu>
2012-12-14 15:50:00 -0500

    pass username to all templates fix

--- myapp/views/home.py
+++ myapp/views/home.py
@@ -1,9 +1,10 @@
-import webapp2
 from google.appengine.ext.webapp import template
+from myapp.views.core import *
 
-class SplashHandler(webapp2.RequestHandler):
+class SplashHandler(BaseHandler):
     def get(self):
-        self.response.write(template.render('templates/home.html', { }))
+        # TODO fix code dup with username and templates
+        self.response.write(template.render('templates/home.html', { 'username': self.session.get("username") }))
 
     def how_it_works(self):
-        self.response.write(template.render('templates/powerpoint.html', { }))
+        self.response.write(template.render('templates/powerpoint.html', { 'username': self.session.get("username")  }))
