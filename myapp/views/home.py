b2d34772bf0d0850a60867e2f02b93710fc73d77
Sara Itani <itani@mit.edu>
2012-12-14 15:36:57 -0500

    modified homepage

--- myapp/views/home.py
+++ myapp/views/home.py
@@ -3,7 +3,7 @@ from google.appengine.ext.webapp import template
 
 class SplashHandler(webapp2.RequestHandler):
     def get(self):
-        self.redirect_to('login')
+        self.response.write(template.render('templates/home.html', { }))
 
     def how_it_works(self):
         self.response.write(template.render('templates/powerpoint.html', { }))
